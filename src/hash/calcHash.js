1ba8b142b5f956c71a3d230424a5f02ffa5d9712
UladzislauN <novikovvlad3005@gmail.com>
2024-01-29 19:31:52 +0300

    feat: implement hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import fs from "fs";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { createHash } from 'crypto';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const calculateHash = async () => {
-    // Write your code here 
+    const readableStream = fs.createReadStream(path.join(__dirname, 'files', 'fileToCalculateHashFor.txt'));
+    let fileContent = '';
+
+    readableStream.on('data', (data) => { 
+        fileContent += data.toString(); 
+    });
+
+    readableStream.on('end', () => { 
+        console.log(createHash('sha256').update(fileContent).digest('hex'));
+    });
 };
 
 await calculateHash();
